0a05b95ce824aa43e19b562ddb52cc462a710109
Ilovepatatos <gabriel@khome.org>
2024-02-04 15:32:53 -0500

    Create DiscordExt.cs

---
+++ src/DiscordExt/DiscordExt.cs
@@ -0,0 +1,19 @@
+﻿using JetBrains.Annotations;
+using Oxide.Core;
+using Oxide.Core.Extensions;
+
+namespace Oxide.Ext.Discord;
+
+[UsedImplicitly]
+public class DiscordExt : Extension
+{
+    private static readonly VersionNumber s_extensionVersion = new(1, 0, 0);
+
+    public override string Name => "Discord";
+    public override string Author => "Ilovepatatos";
+    public override VersionNumber Version => s_extensionVersion;
+
+    public override bool SupportsReloading => true;
+
+    public DiscordExt(ExtensionManager manager) : base(manager) { }
+}
